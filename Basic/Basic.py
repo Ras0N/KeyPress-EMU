26c07ee88ccc7fb3560b55fc80b60f3dae978151
Ras0N <378907175@qq.com>
2017-03-02 13:16:07 +0800

    加入管理员检测

--- Basic/Basic.py
+++ Basic/Basic.py
@@ -4,5 +4,26 @@ import os
 import sys
 import win32api
 import ctypes
-
-if
+from KeyCode import *
+class Basic:
+    isAdmin = False
+    class SID_IDENTIFIER_AUTHORITY(Structure):
+        _fields_ = {("Value",ctypes.wintypes.BYTE * 6)}
+    def __init__():
+        isAdmin = False
+    def isAdmin():
+        #from http://bbs.csdn.net/topics/391025033
+        hAccessToken = 0
+        InfoBuffer = None
+        ptgGroups = None
+        dwInfoBufferSize = 0
+        psidAdministrators = None
+        siaNtAuthority = SID_IDENTIFIER_AUTHORITY()
+        i = 0
+        bRet = False
+        
+class KeyFunction(Basic):
+    def KeyPress(Key,count):
+        for i in range(count):
+            win32api.keybd_event(Key,0x45,KEYEVENTF_EXTENDEDKEY | 0,0);
+            win32api.keybd_event(Key,0x45,KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP,0);
