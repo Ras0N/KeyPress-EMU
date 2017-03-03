d84960f5fa08e2866b136571276b6e02e4251466
Ras0N <378907175@qq.com>
2017-03-03 13:14:16 +0800

    权限获取问题解决

--- Basic/Basic.py
+++ Basic/Basic.py
@@ -4,24 +4,23 @@ import os
 import sys
 import win32api
 import ctypes
+import _winreg
 from KeyCode import *
 class Basic:
     isAdmin = False
-    class SID_IDENTIFIER_AUTHORITY(Structure):
-        _fields_ = {("Value",ctypes.wintypes.BYTE * 6)}
-    def __init__():
-        isAdmin = False
-    def isAdmin():
-        #from http://bbs.csdn.net/topics/391025033
-        hAccessToken = 0
-        InfoBuffer = None
-        ptgGroups = None
-        dwInfoBufferSize = 0
-        psidAdministrators = None
-        siaNtAuthority = SID_IDENTIFIER_AUTHORITY()
-        i = 0
-        bRet = False
-        
+    def __init__(self):
+        self.isAdmin = self.__bisAdmin()
+    def __bisAdmin(self):
+        hKey = (ctypes.wintypes.HKEY)()
+        Advapi = ctypes.windll.LoadLibrary("Advapi32.dll")
+        Advapi.RegOpenKeyExW.argtypes=[wintypes.ULONG,wintypes.LPCWSTR,wintypes.c_int,wintypes.DWORD,wintypes.c_void_p]
+        bRes = Advapi.RegOpenKeyExW(_winreg.HKEY_LOCAL_MACHINE,r"Software\\Microsoft\\Windows NT\\CurrentVersion\\",0,_winreg.KEY_READ|_winreg.KEY_WRITE,byref(hKey))
+        Advapi.RegCloseKey.argtypes=[wintypes.HKEY]
+        Advapi.RegCloseKey(hKey)
+        if bRes != 0:
+            return False
+        else :
+            return True
 class KeyFunction(Basic):
     def KeyPress(Key,count):
         for i in range(count):
