364dc6b7aee14092f9b39f6cb36187b6f68214f7
Ras0N <378907175@qq.com>
2017-03-04 22:00:20 +0800

    实现一部分功能，修复部分Bug

--- Basic/Basic.py
+++ Basic/Basic.py
@@ -22,7 +22,34 @@ class Basic:
         else :
             return True
 class KeyFunction(Basic):
-    def KeyPress(Key,count):
+    def __init__(self):
+        Basic.__init__(self)
+        if self.isAdmin == False:
+            print "[*][WARING] You're not running this script as Administrator,some function may not work properly."
+    def Ascii2VirtualKey(self,Key):
+        return win32api.VkKeyScan(Key)
+    def KeyPress(self,Key,count = 1):
+        if  isinstance(Key,str) and len(Key) == 1:
+            Key = self.Ascii2VirtualKey(Key)
+        elif not isinstance(Key,int):
+            print "[*][Err] Type Error!"
+            return
         for i in range(count):
             win32api.keybd_event(Key,0x45,KEYEVENTF_EXTENDEDKEY | 0,0);
             win32api.keybd_event(Key,0x45,KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP,0);
+    def KeyHold(self,Key):
+        if  isinstance(Key,str) and len(Key) == 1:
+            Key = self.Ascii2VirtualKey(Key)
+        elif not isinstance(Key,int):
+            print "[*][Err] Type Error!"
+            return
+        win32api.keybd_event(Key,0x45,KEYEVENTF_EXTENDEDKEY | 0,0);
+        #win32api.keybd_event(Key,0x45,KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP,0);
+    def KeyRelease(self,Key):
+        if  isinstance(Key,str) and len(Key) == 1:
+            Key = self.Ascii2VirtualKey(Key)
+        elif not isinstance(Key,int):
+            print "[*][Err] Type Error!"
+            return
+        #win32api.keybd_event(Key,0x45,KEYEVENTF_EXTENDEDKEY | 0,0);
+        win32api.keybd_event(Key,0x45,KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP,0);        
